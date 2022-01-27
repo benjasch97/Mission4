3e949b3572e644a24ab809ded9916a11d8168331
benjasch97 <schneider.benjamina97@gmail.com>
2022-01-26 23:53:23 -0700

    Add project files.

---
+++ Mission4/Models/MovieResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+// This contains the proper column headings with the correct requirements. For some reason, when I enter data, it is not displaying a warning properly...
+
+namespace Mission4.Models
+{
+    public class MovieResponse
+    {
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string Borrower { get; set; }
+        [MaxLength(25)]
+        public string Notes { get; set; }
+
+    }
+}
